d17785828a22522b5b16ac78ed4303a91ddcd614
sqfav <158740950+IgorMozzy@users.noreply.github.com>
2025-08-02 10:04:35 +0200

    Практикуемся в написании API автотестов. Часть 1. Практика написания негативных API автотестов для работы с файлами

--- tools/assertions/files.py
+++ tools/assertions/files.py
@@ -67,7 +67,6 @@ def assert_create_file_with_empty_filename_response(actual: ValidationErrorRespo
     )
     assert_validation_error_response(actual, expected)
 
-
 def assert_create_file_with_empty_directory_response(actual: ValidationErrorResponseSchema):
     """
     Проверяет, что ответ на создание файла с пустым значением директории соответствует ожидаемой валидационной ошибке.
@@ -83,6 +82,32 @@ def assert_create_file_with_empty_directory_response(actual: ValidationErrorResp
                 context={"min_length": 1},  # Минимальная длина строки должна быть 1 символ.
                 message="String should have at least 1 character",  # Сообщение об ошибке.
                 location=["body", "directory"]  # Ошибка возникает в теле запроса, поле "directory".
+            )
+        ]
+    )
+    assert_validation_error_response(actual, expected)
+
+def assert_get_file_with_incorrect_file_id_response(actual: ValidationErrorResponseSchema):
+    """
+    Проверяет, что ответ API на запрос файла с некорректным file_id
+    соответствует ожидаемому формату ошибки валидации.
+
+    :param actual: Фактический ответ API с ошибкой валидации
+    :raises AssertionError: Если фактический ответ не соответствует ожидаемому
+    """
+    expected = ValidationErrorResponseSchema(
+        details=[
+            ValidationErrorSchema(
+                type="uuid_parsing",
+                input="incorrect-file-id",
+                context={
+                    "error": "invalid character: expected an optional prefix of `urn:uuid:` "
+                             "followed by [0-9a-fA-F-], found `i` at 1"
+                },
+                message="Input should be a valid UUID, invalid character: "
+                        "expected an optional prefix of `urn:uuid:` "
+                        "followed by [0-9a-fA-F-], found `i` at 1",
+                location=["path", "file_id"]
             )
         ]
     )
